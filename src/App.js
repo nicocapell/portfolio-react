f28854726ab207f14efa03e6a5fb8abbf8063703
nicocapell <84024360+nicocapell@users.noreply.github.com>
2022-02-02 17:23:59 -0300

    Nav + About

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import Cover from "./components/cover/Cover";
 import Navbar from './components/navbar/Navbar';
+import About from './components/about/About';
 
 
 function App() {
@@ -23,11 +24,10 @@ function App() {
     <div className="App">
       <Navbar isScrolling={scrollHeight}/>
       <Cover />
+      <About/>
 
 
-      <div>
-      <h1> lorem isassssssssssadsssssssssssssssssssssssssssssssssssssssssss;fdavgsdavsdpvlapsvpsflavpl</h1>
-    </div>
+  
 
  
     </div>
---
+++ src/components/about/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './About.css'
+
+const About = () => {
+  return (
+  
+  <div className='about-container'>
+      <div className='about-desc'>
+          <h3>
+              Let me tell you something about my Lagarta
+          </h3>
+          <p> 
+          </p>
+      </div>
+      <div className='about-img'> 
+       <img src="https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358_960_720.jpg" alt="about" />
+
+      </div>
+
+
+  </div>
+  
+  );
+};
+
+export default About;
